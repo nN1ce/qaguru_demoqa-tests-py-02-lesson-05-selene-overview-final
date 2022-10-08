0b8956edef20f379a510376b6f2daba1f9977563
nN1ce <nicevector@yandex.ru>
2022-10-08 21:12:39 +0300

    add_lesson_05

---
+++ demoqa_tests/utiils/files.py
@@ -0,0 +1,8 @@
+
+#
+# def abs_path_from_project_root(relative_path: str):
+#     # TODO: implement
+#     ...
+
+def abspath_photo(reletive_path):
+    ...
---
+++ tests/test_text_box.py
@@ -0,0 +1,23 @@
+from selene.support.shared import browser
+from selene import have, be
+from selene.support import by
+
+from demoqa_tests.model import app
+# from demoqa_tests.utiils import files
+
+
+def test_submit_user_details():
+    app.given_opened_text_box_page()
+
+    browser.should(have.title('ToolsQA'))
+    browser.element('.main-header').should(have.text('Text Box'))
+    browser.element(by.text('Text Box')).should(be.visible)
+
+    browser.element('#userForm').all('input, textarea').should(have.size(4))
+    browser.all('.form-label').element_by(have.text('Address')).should(have.text('Current Address'))
+
+    browser.element('#userName').type('Evgen')
+
+    submit = browser.element('#submit').click()
+
+    # browser.element('#uploadFile').send_keys(files.abs_path_from_project_root('resources/temp.txt'))
