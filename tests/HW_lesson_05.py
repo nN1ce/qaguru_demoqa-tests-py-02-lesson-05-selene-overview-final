8d0f028b9b689e84223c438472af8cedfad8daab
nN1ce <nicevector@yandex.ru>
2022-12-11 08:14:58 +0300

    add blackd

--- tests/HW_lesson_05.py
+++ tests/HW_lesson_05.py
@@ -5,7 +5,7 @@ from selene.support.shared import browser
 
 
 def test_practice_form():
-    browser.open('https://demoqa.com/automation-practice-form')
+    browser.open('/automation-practice-form')
     browser.element('#firstName').type('Evgeniy')
     browser.element('#lastName').type('Lukyanov')
     browser.element('#userEmail').type('evgeniy@gmail.qa')
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,12 @@
+import os
+
 import pytest
 from selene.support.shared import browser
 
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_management_for_hw():
-    browser.config.base_url = 'https://demoqa.com/automation-practice-form'
+    browser.config.base_url = os.getenv('selene.base_url', 'https://demoqa.com')
     browser.config.timeout = 3
     # browser.config.browser_name = 'chrome' # and 'firefox' and 'edge' #or 'opera'
     browser.config.window_width = 1920
